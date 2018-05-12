950adf7d1fae696eac67c7522126ac62afe78bd4
mengqingwei <1973154337@qq.com>
2018-05-12 08:24:55 +0800

    Initial commit

---
+++ MyServlet.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2018/5/11.
+ */
+public class MyServlet extends HttpServlet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do a get method");
+//        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
